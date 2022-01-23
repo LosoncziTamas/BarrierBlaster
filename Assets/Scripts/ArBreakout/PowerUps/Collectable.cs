df43c4b38e1757c121aced4ae9d1ce51748a101a
Tamás Losonczi <losonczi.tamas91@gmail.com>
2022-01-23 17:41:01 +0100

    immrpove collectable animations

--- Assets/Scripts/ArBreakout/PowerUps/Collectable.cs
+++ Assets/Scripts/ArBreakout/PowerUps/Collectable.cs
@@ -1,9 +1,10 @@
 using ArBreakout.GamePhysics;
+using ArBreakout.Misc;
+using DG.Tweening;
 using UnityEngine;
 
 namespace ArBreakout.PowerUps
 {
-    [RequireComponent(typeof(Collider))]
     public class Collectable : MonoBehaviour
     {
         public const string GameObjectTag = "Collectable";
@@ -14,8 +15,14 @@ namespace ArBreakout.PowerUps
         
         private Vector3 _velocity;
         private Vector3 _acceleration;
-
+        private Vector3 _originalScale;
+        
         private bool _destroyed;
+
+        private void Awake()
+        {
+            _originalScale = transform.localScale;
+        }
 
         public void Init(PowerUp powerUp)
         {
@@ -35,6 +42,22 @@ namespace ArBreakout.PowerUps
 
             _velocity += BreakoutPhysics.CalculateVelocityDelta(_acceleration);
             transform.localPosition += BreakoutPhysics.CalculateMovementDelta(_acceleration, _velocity);
+        }
+
+        private void OnGUI()
+        {
+            GUILayout.Space(100);
+            if (GUILayout.Button("Animate"))
+            {
+                AnimateAppearance();
+            }
+        }
+
+        public void AnimateAppearance()
+        {
+            transform.localScale = Vector3.one * 0.1f;
+            transform.AnimatePunchScale(_originalScale * 0.9f, Ease.Linear, 0.4f);
+            transform.DOPunchRotation(Vector3.forward * 360f, 1.0f, 1, 0.5f);
         }
 
         public void Destroy()
