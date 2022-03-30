cfae3568210e7522db1fbfdfdc3b4db631db60db
Tamás Losonczi <losonczi.tamas91@gmail.com>
2022-03-30 08:13:16 +0200

    asset purge

--- Assets/Scripts/ArBreakout/Levels/LevelData.cs
+++ Assets/Scripts/ArBreakout/Levels/LevelData.cs
@@ -1,7 +1,5 @@
-using System;
 using System.Collections.Generic;
 using ArBreakout.Game.Bricks;
-using Array2DEditor;
 using UnityEngine;
 
 namespace ArBreakout.Levels
@@ -9,22 +7,16 @@ namespace ArBreakout.Levels
     [CreateAssetMenu]
     public class LevelData : ScriptableObject
     {
-        [SerializeField] private Array2DString _layout;
-        [SerializeField] private Array2DColor _colors;
         [SerializeField] private float _timeLimit;
         [SerializeField] private string _name;
         [SerializeField] private bool _unlocked;
-
-        public Array2DString Layout => _layout;
-
+        
         public float TimeLimit => _timeLimit;
 
         public string Name => _name;
 
         public bool Unlocked => _unlocked;
-
-        public Array2DColor Colors => _colors;
-
+        
         public int Id => GetInstanceID();
 
         public List<BrickAttributes> BrickAttributes = new();
