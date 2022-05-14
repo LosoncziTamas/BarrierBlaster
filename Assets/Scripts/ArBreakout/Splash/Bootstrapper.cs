ef76c90796d19921c50b7d49e176953e2c6d2984
Tamás Losonczi <losonczi.tamas91@gmail.com>
2022-05-14 11:58:27 +0200

    tweak splash screen

--- Assets/Scripts/ArBreakout/Splash/Bootstrapper.cs
+++ Assets/Scripts/ArBreakout/Splash/Bootstrapper.cs
@@ -1,4 +1,6 @@
+using System;
 using System.Collections;
+using DG.Tweening;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
@@ -6,13 +8,26 @@ namespace ArBreakout.Splash
 {
     public class Bootstrapper : MonoBehaviour
     {
+        private const int GameSceneIndex = 1;
+
+        [SerializeField] private CanvasGroup _splash;
+
+        private void Awake()
+        {
+            _splash.alpha = 0;
+            DontDestroyOnLoad(this);
+        }
+
         private IEnumerator Start()
         {
-            var loadScene = SceneManager.LoadSceneAsync(1, LoadSceneMode.Additive);
-            while (loadScene.progress < 99)
+            var loadScene = SceneManager.LoadSceneAsync(GameSceneIndex, LoadSceneMode.Additive);
+            loadScene.allowSceneActivation = false;
+            // TODO: fade
+            while (loadScene.progress < 0.9f)
             {
                 yield return null;
             }
+            loadScene.allowSceneActivation = true;
         }
     }
 }
